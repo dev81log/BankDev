5251d0025f2d04edafdcbc1933d341fe27fa8b52
dev81log <dev81log@gmail.com>
2022-05-31 19:56:30 -0300

    construindo menu e visualizações do cleinte

--- Program.cs
+++ Program.cs
@@ -1,3 +1,76 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections;
+using Array = System.Array;
 
-Console.WriteLine("Hello, World!");
+namespace BankDev
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Window();
+            Menu();
+        }
+
+        private static void Window()
+        {
+            Console.WriteLine("**************************************");
+            Console.WriteLine("*** BankDev - Electronic Terminal ****");
+            Console.WriteLine("**************************************");
+            
+            AccountList();
+
+            Console.WriteLine("1 - Balance");
+            Console.WriteLine("2 - Withdraw");
+            Console.WriteLine("3 - Exit Service");
+        }
+
+        private static void Menu()
+        {
+            var choiceInput = short.Parse(Console.ReadLine() ?? string.Empty);
+
+            switch (choiceInput)
+            {
+                case 1:
+                    Console.WriteLine("Balance");
+                    break;
+                case 2:
+                    Console.WriteLine("Withdraw");
+                    break;
+                case 3:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    return;
+            }
+        }
+
+        private static void Operations()
+        {
+        }
+
+        private static ArrayList ListUser()
+        {
+            var accountList = new ArrayList
+            {
+                22,
+                "Robert",
+                true
+            };
+            return accountList;
+        }
+
+        private static void AccountList()
+        {
+            var client = ListUser();
+            Console.WriteLine($"Welcome {client[1]} to id {client[0]} and service {client[2]}");
+
+        }
+
+        struct AccountListClient
+        {
+            public int ClientId { get; set; }
+            public string ClientName { get; set; }
+        }
+    }
+}
